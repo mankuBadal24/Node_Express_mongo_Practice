cc137df59ea8aad9c170f4c3ece8d40189db0749
mankuBadal24 <144877956+mankuBadal24@users.noreply.github.com>
2025-07-10 01:14:43 +0530

    middlewares

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const app = express()
 
 // routes using expresss
 //FUN FACT == behind the scene express also uses http 😂
+// app.get('/',(req,res)=>{
+//     res.send("homepage")
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send("about")
+// })
+
+
+// now using express js to render html page in routes
+app.set("view engine",'ejs')
+
+
+// middlewares = = 
+app.use((req,res,next)=>{
+    console.log(" this is middleware ");
+    return next()
+})
+
+
 app.get('/',(req,res)=>{
-    res.send("homepage")
+    res.render("index")
 })
 
 app.get('/about',(req,res)=>{
